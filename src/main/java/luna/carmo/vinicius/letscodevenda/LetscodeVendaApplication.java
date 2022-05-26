384d100198e133d67bf1100b980c8b13f6c7a499
Vinicius Luna <viniciuscluna@gmail.com>
2022-05-26 19:06:31 -0300

    Initial Commit

---
+++ src/main/java/luna/carmo/vinicius/letscodevenda/LetscodeVendaApplication.java
@@ -0,0 +1,13 @@
+package luna.carmo.vinicius.letscodevenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetscodeVendaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LetscodeVendaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/luna/carmo/vinicius/letscodevenda/LetscodeVendaApplicationTests.java
@@ -0,0 +1,13 @@
+package luna.carmo.vinicius.letscodevenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LetscodeVendaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
