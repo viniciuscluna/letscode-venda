35988f9d84fdd122cafe75b90912c34a8a8ad732
Vinicius Luna <viniciuscluna@gmail.com>
2022-05-28 19:31:44 -0300

    Added Eureka

--- src/main/java/luna/carmo/vinicius/letscodevenda/LetscodeVendaApplication.java
+++ src/main/java/luna/carmo/vinicius/letscodevenda/LetscodeVendaApplication.java
@@ -2,8 +2,10 @@ package luna.carmo.vinicius.letscodevenda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class LetscodeVendaApplication {
 
     public static void main(String[] args) {
