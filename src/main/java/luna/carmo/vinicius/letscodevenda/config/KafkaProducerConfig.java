47eeca9b0ff054fb6c3e0e074738fff2484244b0
Vinicius Luna <viniciuscluna@gmail.com>
2022-05-28 19:51:05 -0300

    Comented Kafka

--- src/main/java/luna/carmo/vinicius/letscodevenda/config/KafkaProducerConfig.java
+++ src/main/java/luna/carmo/vinicius/letscodevenda/config/KafkaProducerConfig.java
@@ -14,25 +14,25 @@ import java.util.Map;
 
 @Configuration
 public class KafkaProducerConfig {
-
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootsrapServers;
-
-    public Map<String, Object> producerConfig() {
-        HashMap<String, Object> props = new  HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootsrapServers);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return props;
-    }
-
-    @Bean
-    public ProducerFactory<String, String> producerFactory(){
-        return new DefaultKafkaProducerFactory<>(producerConfig());
-    }
-
-    @Bean
-    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory){
-        return new KafkaTemplate<>(producerFactory);
-    }
+//
+//    @Value("${spring.kafka.bootstrap-servers}")
+//    private String bootsrapServers;
+//
+//    public Map<String, Object> producerConfig() {
+//        HashMap<String, Object> props = new  HashMap<>();
+//        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootsrapServers);
+//        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+//        return props;
+//    }
+//
+//    @Bean
+//    public ProducerFactory<String, String> producerFactory(){
+//        return new DefaultKafkaProducerFactory<>(producerConfig());
+//    }
+//
+//    @Bean
+//    public KafkaTemplate<String, String> kafkaTemplate(ProducerFactory<String, String> producerFactory){
+//        return new KafkaTemplate<>(producerFactory);
+//    }
 }
